7b7417b1a1e325c34dc7dd00dde046d0f8f091ba
samgill172 <robertwilk916@gmail.com>
2019-06-26 14:46:23 -0700

    forms

--- src/app/scripts/scripts.component.ts
+++ src/app/scripts/scripts.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scripts.component.css']
 })
 export class ScriptsComponent implements OnInit {
+  name: string;
+  email: string;
+  uses = [""]
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form) {
+    console.log(`Bound {Name: ${this.name}, Email: ${this.email}}`);
+    console.log(`Formed {Name: ${form.name}, Email: ${form.email}}`);
   }
 
 }
